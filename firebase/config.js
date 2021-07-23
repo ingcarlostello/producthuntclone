2ea88f2aee207ebfb4fbdfaffb6d56545b8c0bdb
Ing Carlos Tello <ingcarlostello@hotmail.com>
2021-07-23 02:33:07 -0500

    product hunt clone ended

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVZSHUnP77iFTH4jTjjvklUGGRfQltp8Q",
+    authDomain: "product-hunt-clone-3dbbb.firebaseapp.com",
+    projectId: "product-hunt-clone-3dbbb",
+    storageBucket: "product-hunt-clone-3dbbb.appspot.com",
+    messagingSenderId: "360398611186",
+    appId: "1:360398611186:web:a057b63389df8ac2cba66b"
+};
+
+export default firebaseConfig;
